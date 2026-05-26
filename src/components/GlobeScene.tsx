cacf41e08c25c0a6bd197b3651c844bb9b5bbbe6
aliorbz <shrakib155@gmail.com>
2026-05-26 18:58:35 +0600

    style: reduce country border opacity by 70% for a fainter cyber aesthetic

--- src/components/GlobeScene.tsx
+++ src/components/GlobeScene.tsx
@@ -200,7 +200,7 @@ export default function GlobeScene({ pins, selectedPin, onSelectPin }: GlobeScen
         polygonsData={countries}
         polygonCapColor={() => 'rgba(0, 255, 102, 0.08)'} // Land part a bit greenish (very transparent green overlay)
         polygonSideColor={() => 'rgba(0, 0, 0, 0)'}
-        polygonStrokeColor={() => '#00ff66'} // Thin neon green line separating countries
+        polygonStrokeColor={() => 'rgba(0, 255, 102, 0.3)'} // Thin neon green line separating countries (30% opacity)
         polygonAltitude={0.006}
         onPolygonClick={() => onSelectPin(null)}
 
